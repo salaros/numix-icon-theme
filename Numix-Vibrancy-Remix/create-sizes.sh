6f24e6233d1e1358a25b4d73e2b1df2e925256ce
salaros <salaros@yandex.ru>
2017-10-09 02:19:35 +0300

    process .sym files (copy symlinks to "size" folders)

--- Numix-Vibrancy-Remix/create-sizes.sh
+++ Numix-Vibrancy-Remix/create-sizes.sh
@@ -3,8 +3,19 @@
 shopt -u dotglob
 
 for size in 256 128 96 64 32 24 22 16; do
+	# Resize icons and copy resized icons to "size" folders
 	for file in source/*.png; do
 		icon="$(basename $file)"
 		convert -verbose -resize ${size}x${size} $file $size/apps/$icon
 	done;
+	
+	
+	# Copy symlinks to each "size" folder
+	for symlink in source/*.sym; do
+		filename="${symlink%.*}"
+		filename="$(basename $filename)"
+		rm -rfv $size/apps/$filename
+		cp -Pv $symlink $size/apps/$filename
+	done;
+
 done
